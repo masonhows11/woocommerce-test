fcb156207d6693efe8eb4f8db5ae441ee2304922
mason.hows11 <mason.hows11@gmail.com>
2024-08-16 18:11:53 +0330

    customize single product tab section updated

--- wp-content/themes/clab/woocommerce/single-product/tabs/tabs.php
+++ wp-content/themes/clab/woocommerce/single-product/tabs/tabs.php
@@ -31,24 +31,40 @@ $product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
 if ( ! empty( $product_tabs ) ) : ?>
 
 	<div class="col-md-12">
+
 		<ul class="nav nav-line mb-md-5 mb-3" role="tablist">
 			<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
-				<li class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
-					<a href="#tab-<?php echo esc_attr( $key ); ?>">
+				<li
+					class="nav-item <?php echo esc_attr( $key ); ?>_tab"
+					id="tab-title-<?php echo esc_attr( $key ); ?>"
+					role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
+					<a
+						class="nav-link"
+						data-toggle="tab"
+						aria-selected="true"
+						role="tab"
+						aria-controls="<?php echo esc_attr( $key ); ?>"
+						href="#tab-<?php echo esc_attr( $key ); ?>">
 						<?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
 					</a>
 				</li>
 			<?php endforeach; ?>
 		</ul>
-		<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
-			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
-				<?php
-				if ( isset( $product_tab['callback'] ) ) {
-					call_user_func( $product_tab['callback'], $key, $product_tab );
-				}
-				?>
-			</div>
-		<?php endforeach; ?>
+		<div class="tab-content text-left">
+			<?php $counter = 0; ?>
+			<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+				<div class="tab-pane fade <?php echo $counter == 0 ? 'active show' : '' ?>"
+					 id="tab-<?php echo esc_attr( $key ); ?>"
+					 role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
+					<?php
+					if ( isset( $product_tab['callback'] ) ) {
+						call_user_func( $product_tab['callback'], $key, $product_tab );
+					}
+					?>
+				</div>
+			<?php $counter++; endforeach; ?>
+		</div>
+
 		<?php do_action( 'woocommerce_product_after_tabs' ); ?>
 	</div>
 
