399881fb032c9ff66a9559d6df05f19fef045669
mason.hows11 <mason.hows11@gmail.com>
2024-08-16 18:18:49 +0330

    customize single product tab product attr section updated

--- wp-content/themes/clab/woocommerce/single-product/product-attributes.php
+++ wp-content/themes/clab/woocommerce/single-product/product-attributes.php
@@ -23,7 +23,10 @@ if ( ! $product_attributes ) {
 	return;
 }
 ?>
-<table class="woocommerce-product-attributes shop_attributes">
+<div class="table-responsive">
+
+</div>
+<table class="table table-striped table-bordered">
 	<?php foreach ( $product_attributes as $product_attribute_key => $product_attribute ) : ?>
 		<tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr( $product_attribute_key ); ?>">
 			<th class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
