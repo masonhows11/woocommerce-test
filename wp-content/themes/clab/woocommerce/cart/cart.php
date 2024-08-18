5fe1f487138cd5db159a3ab26a3062dc980a2320
mason.hows11 <mason.hows11@gmail.com>
2024-08-18 22:23:08 +0330

    cart template price section updated

--- wp-content/themes/clab/woocommerce/cart/cart.php
+++ wp-content/themes/clab/woocommerce/cart/cart.php
@@ -175,31 +175,31 @@ do_action( 'woocommerce_before_cart' ); ?>
 					<?php do_action( 'woocommerce_after_cart_contents' ); ?>
 					</tbody>
 				</table>
+				<?php do_action( 'woocommerce_after_cart_table' ); ?>
 			</div>
 		</div>
 	</div>
 
 	<div class="row">
-		<div class="col-md-6">
-			<?php if ( wc_coupons_enabled() ) { ?>
-				<div class="coupon">
-					<label for="coupon_code" class="screen-reader-text"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
+
+		<div class="col-md-7">
+			<h6>کد تخفیف</h6>
+			<p class="text-muted">لطفا کد کوپن خود را در صورت لزوم وارد کنید</p>
+			<form class="d-md-flex mt-lg-4 mt-3 mb-4">
+				<?php if ( wc_coupons_enabled() ) { ?>
+					<label for="coupon_code" class="screen-reader-text"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label>
+					<input type="text" name="coupon_code" class="form-control mr-2 mb-2" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" />
+					<button type="submit" class="btn btn-pill btn-solid-dark mr-2  mb-2" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
 					<?php do_action( 'woocommerce_cart_coupon' ); ?>
-				</div>
-			<?php } ?>
-
-			<button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
-
-			<?php do_action( 'woocommerce_cart_actions' ); ?>
-
-			<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
+				<?php } ?>
+				<button type="submit" class="btn btn-pill btn-outline  mb-2" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
+				<?php do_action( 'woocommerce_cart_actions' ); ?>
+				<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
+			</form>
 		</div>
-		<div class="col-md-6">
-			<?php do_action( 'woocommerce_after_cart_table' ); ?>
-
-
+		
+		<div class="col-md-5">
 			<?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
-
 			<div class="cart-collaterals">
 				<?php
 				/**
