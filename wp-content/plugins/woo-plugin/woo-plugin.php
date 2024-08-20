8cbd9c605cbf60840001e3b5c46ccf95286e71fb
mason.hows11 <mason.hows11@gmail.com>
2024-08-20 17:03:08 +0330

    add plugin for woocommerce

---
+++ wp-content/plugins/woo-plugin/woo-plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: Woo plugin
+ * Plugin URI: https://websolo.ir/
+ * Description: a woocommerce sample plugin
+ * Version: 9.1.4
+ * Author: Naeem soltany
+ * Author URI: https://websolo.ir/
+ * Text Domain: woo-plugin
+ * Domain Path: /languages/
+ * Requires at least: 6.5
+ * Requires PHP: 7.4
+ *
+ */
+
+if(!defined("ABSPATH"))
+{
+    exit;
+}
