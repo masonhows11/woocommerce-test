6e6eef790b8e43132c99eb46d9a89c29dc8eb99c
mason.hows11 <mason.hows11@gmail.com>
2024-08-21 21:10:48 +0330

    menu style options-panel updated

--- wp-content/themes/clab/options-panel/views/index.php
+++ wp-content/themes/clab/options-panel/views/index.php
@@ -2,7 +2,28 @@
     <h2>تنظیمات قالب Clab</h2>
     <div class="container">
 
-        
+        <div class="options-menu">
+            <ul>
+                <li>
+                    <a href="">عمومی</a>
+                </li>
+                <li>
+                    <a href="">مطالب</a>
+                </li>
+                <li>
+                    <a href="">تصاویر</a>
+                </li>
+                <li>
+                    <a href="">اطلاع رسانی</a>
+                </li>
+
+            </ul>
+        </div>
+
+        <div class="options-content">
+
+        </div>
+
 
     </div>
 </div>
