d56cdb3e4d9202208e9e69b33d8b796a6f812146
mason.hows11 <mason.hows11@gmail.com>
2024-08-21 20:42:50 +0330

    clab options panel initial updated

--- wp-content/themes/clab/options-panel/index.php
+++ wp-content/themes/clab/options-panel/index.php
@@ -1,5 +1,19 @@
 <?php 
 
+// add assets
+add_action('admin_enqueue_scripts','clab_options_panel_assets');
+function clab_options_panel_assets(){
+
+    // add & reg css files
+    wp_register_style('options-panel-style',CLAB_URL.'options-panel/assets/css/options.css');
+    wp_enqueue_style('options-panel-style');
+
+    // add & reg js files
+    wp_register_script('options-panel-script',CLAB_URL.'options-panel/assets/js/options.js',['jquery']);
+    wp_enqueue_script('options-panel-script');
+
+}
+// add options-panel
 add_action('admin_menu','clab_add_option_panel');
 function clab_add_option_panel()
 {
