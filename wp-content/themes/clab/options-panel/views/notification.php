f5d6fbf141d0b141d6fcfe48fa76da0dedc1d8cb
mason.hows11 <mason.hows11@gmail.com>
2024-08-22 23:14:10 +0330

    options-panel notification option form implemented

--- wp-content/themes/clab/options-panel/views/notification.php
+++ wp-content/themes/clab/options-panel/views/notification.php
@@ -1,1 +1,24 @@
-<h2>notification</h2>
+<div class="options-form">
+
+    <fieldset class="posts-option-fieldset">
+        <legend>متن ایمیل بعد از ثبت نام کاربر:</legend>
+
+
+        <div class="form-row">
+            <label for="email-title">عنوان ایمیل :</label>
+            <input type="text" name="new_user_email_title" id="email-title" value="<?php  echo esc_attr(!empty($options['new_user_email_title']) ? $options['new_user_email_title'] : '');  ?>">
+        </div>
+
+        <div class="form-row">
+            <label for="email-body">متن ایمیل :</label>
+            <textarea rows="10" name="new_user_email_body" id="email-body">
+                <?php  echo esc_attr(!empty($options['new_user_email_body']) ? $options['new_user_email_body'] : '');  ?>
+        </textarea>
+        </div>
+
+        <div class="form-row">
+            <button type="submit" class="button" name="save_options">ذخیره سازی</button>
+        </div>
+    </fieldset>
+
+</div>
