8c27e45437e06eb17efb487dac7cdb05b6f5aa5f
mason.hows11 <mason.hows11@gmail.com>
2024-08-22 10:27:14 +0330

    options-panel general page style & forms updated 80%

--- wp-content/themes/clab/options-panel/views/general.php
+++ wp-content/themes/clab/options-panel/views/general.php
@@ -1,12 +1,17 @@
 <div class="options-form">
 
     <div class="form-row">
-        <label for="site-title">عنوان سایت</label>
+        <label for="site-title">عنوان سایت :</label>
         <input type="text" name="site_title" id="site-title" placeholder="عنوان سایت را وارد کنید">
     </div>
 
     <div class="form-row">
-         <button type="buttom" class="button" name="site_title">ذخیره سازی</button>
+        <label for="home-posts-count">تعداد مطالب در صفحه اول :</label>
+        <input type="number" name="home_posts_count" min="1" max="100" value="10" id="home-posts-count" placeholder="تعداد را وارد کنید">
+    </div>
+
+    <div class="form-row">
+         <button type="submit" class="button" name="site_title">ذخیره سازی</button>
     </div>
 
 </div>
