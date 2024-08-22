7e4f210f5f245d3d1f80c6916b9fd16504bbc2a1
mason.hows11 <mason.hows11@gmail.com>
2024-08-22 10:11:51 +0330

    add general page form  inputs

--- wp-content/themes/clab/options-panel/views/general.php
+++ wp-content/themes/clab/options-panel/views/general.php
@@ -1,1 +1,12 @@
-<h2>General</h2>
+<div class="options-form">
+
+    <div class="form-row">
+        <label for="site-title">عنوان سایت</label>
+        <input type="text" name="site_title" id="site-title" placeholder="عنوان سایت را وارد کنید">
+    </div>
+
+    <div class="form-row">
+         <button type="buttom" class="button" name="site_title">ذخیره سازی</button>
+    </div>
+
+</div>
