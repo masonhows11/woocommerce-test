d8e3fdc721eda083431de54024857cc71530a656
mason.hows11 <mason.hows11@gmail.com>
2024-08-23 05:35:59 +0330

    options-panel right menu items style fixed

--- wp-content/themes/clab/options-panel/views/notification.php
+++ wp-content/themes/clab/options-panel/views/notification.php
@@ -6,13 +6,16 @@
 
         <div class="form-row">
             <label for="email-title">عنوان ایمیل :</label>
-            <input type="text" name="new_user_email_title" id="email-title" value="<?php  echo esc_attr(!empty($options['new_user_email_title']) ? $options['new_user_email_title'] : '');  ?>">
+            <input type="text"
+                   name="new_user_email_title"
+                   id="email-title"
+                   value="<?php  echo isset($options['new_user_email_title']) ? $options['new_user_email_title'] : '';  ?>">
         </div>
 
         <div class="form-row">
             <label for="email-body">متن ایمیل :</label>
             <textarea rows="10" name="new_user_email_body" id="email-body">
-                <?php  echo esc_attr(!empty($options['new_user_email_body']) ? $options['new_user_email_body'] : '');  ?>
+                <?php  echo isset($options['new_user_email_body']) ? $options['new_user_email_body'] : ''  ?>
         </textarea>
         </div>
 
