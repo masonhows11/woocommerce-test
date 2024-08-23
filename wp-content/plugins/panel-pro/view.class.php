8a78a630f57ec0264d3281da099eb572c81f5694
mason.hows11 <mason.hows11@gmail.com>
2024-08-23 20:50:45 +0330

    pro-panel pass data from method to view

--- wp-content/plugins/panel-pro/view.class.php
+++ wp-content/plugins/panel-pro/view.class.php
@@ -4,8 +4,10 @@ class view
 {
 
     // to display html/php file as view
-    public static function load($view)
+    public static function load($view ,$data = [])
     {
+        // to access data variable in view file
+        extract($data);
         include UPP_DIR_VIEW.$view;
     }
 
