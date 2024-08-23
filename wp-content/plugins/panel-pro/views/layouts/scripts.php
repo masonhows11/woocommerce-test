ca24f50e2b6a8928d75681c1dd96069c6cc08551
mason.hows11 <mason.hows11@gmail.com>
2024-08-23 22:18:34 +0330

    pro-panel layouts add seperate files

---
+++ wp-content/plugins/panel-pro/views/layouts/scripts.php
@@ -0,0 +1,38 @@
+<!-- jQuery 3 -->
+<script src="<?php echo UPP_ASSETS_URL; ?>jquery/dist/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?php echo UPP_ASSETS_URL; ?>jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo UPP_ASSETS_URL; ?>bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo UPP_ASSETS_URL; ?>js/adminlte.min.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo UPP_ASSETS_URL; ?>js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo UPP_ASSETS_URL; ?>js/demo.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="<?php echo UPP_ASSETS_URL; ?>bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo UPP_ASSETS_URL; ?>jquery-knob/dist/jquery.knob.min.js"></script>
+
+<!-- Morris.js charts -->
+<!--<script src="bower_components/raphael/raphael.min.js"></script>-->
+<!--<script src="bower_components/morris.js/morris.min.js"></script>-->
+<!-- Sparkline -->
+<!--<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>-->
+<!-- jvectormap -->
+<!--<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>-->
+<!--<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>-->
+<!-- daterangepicker -->
+<!--<script src="bower_components/moment/min/moment.min.js"></script>-->
+<!--<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>-->
+<!-- datepicker -->
+<!--<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>-->
+<!-- Slimscroll -->
+<!--<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>-->
+<!-- FastClick -->
+<!--<script src="bower_components/fastclick/lib/fastclick.js"></script>-->
---
+++ wp-content/plugins/panel-pro/views/layouts/styles.php
@@ -0,0 +1,30 @@
+<!-- Bootstrap 3.3.7 -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>css/bootstrap-theme.css">
+<!-- Bootstrap rtl -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>css/rtl.css">
+<!-- persian Date Picker -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>css/persian-datepicker-0.4.5.min.css">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>font-awesome/css/font-awesome.min.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>Ionicons/css/ionicons.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>css/AdminLTE.css">
+<!-- AdminLTE Skins. Choose a skin from the css/skins
+     folder instead of downloading all of them to reduce the load. -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>css/skins/_all-skins.min.css">
+<!-- Daterange picker -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>bootstrap-daterangepicker/daterangepicker.css">
+<!-- bootstrap wysihtml5 - text editor -->
+<link rel="stylesheet" href="<?php echo UPP_ASSETS_URL; ?>bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+<!-- Morris chart -->
+<!--    <link rel="stylesheet" href="bower_components/morris.js/morris.css">-->
+<!-- jvectormap -->
+<!--    <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">-->
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+
+<![endif]-->
