2f888c3123742f3add1a7ccf69358ddb5831df3d
mason.hows11 <mason.hows11@gmail.com>
2024-08-24 12:46:52 +0330

    pro-panel dashboard section updated 80%

--- wp-content/plugins/panel-pro/panel/handlers/DashboardHandler.php
+++ wp-content/plugins/panel-pro/panel/handlers/DashboardHandler.php
@@ -12,11 +12,29 @@ class DashboardHandler extends Handler {
     public function index()
     {
         $current_user = wp_get_current_user();
-        view::load('panel.dashboard.index',[
-            'current_user' => $current_user
-        ]);
+        $params = [
+          'current_user' => $current_user,
+          'user_posts_count' => count_user_posts($current_user->ID),
+          'user_comments_count' => $this->get_user_comments_count($current_user->ID) ,
+        ];
+        view::load('panel.dashboard.index',$params);
         // the second parameter is variable pass to view file
         // view::load('panel/dashboard/index.php' , ['name' => 'mason' , 'age' => 32]);
         //var_dump('this is index fun in dashboard');
+    }
+
+
+    private function get_user_comments_count($user_id)
+    {
+        global $wpdb;
+
+        $user_comments_count = $wpdb->get_var($wpdb->prepare("
+        SELECT COUNT(comment_ID)
+        FROM {$wpdb->comments}
+        WHERE user_id=%d
+        AND comment_approved = 1
+        ",$user_id));
+        return intval($user_comments_count);
+        
     }
 }
--- wp-content/plugins/panel-pro/views/panel/dashboard/index.php
+++ wp-content/plugins/panel-pro/views/panel/dashboard/index.php
@@ -1,9 +1,19 @@
 <div class="row">
     <div class="panel panel-primary">
-        <div class="panel-heading">داشبورد</div>
+        <div class="panel-heading"><strong>داشبورد</strong></div>
         <div class="panel-body">
             <p><?php echo $current_user->display_name ?></p>
             <p>خوش آمدید</p>
+            <div class="user-state">
+                <p>
+                    <span>تعداد مطالب نوشته شده :</span>
+                    <span><?php echo $user_posts_count?></span>
+                </p>
+                <p>
+                    <span>تعداد کامنت های تایید شده :</span>
+                    <span><?php echo $user_comments_count ?></span>
+                </p>
+            </div>
         </div>
         <div class="panel-footer"></div>
     </div>
