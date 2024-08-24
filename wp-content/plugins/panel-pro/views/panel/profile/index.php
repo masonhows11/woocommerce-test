fa6e0eb3ec2bbb907541140a4562e30d98574f9d
mason.hows11 <mason.hows11@gmail.com>
2024-08-24 15:46:50 +0330

    add profile edit info fomr

--- wp-content/plugins/panel-pro/views/panel/profile/index.php
+++ wp-content/plugins/panel-pro/views/panel/profile/index.php
@@ -1,3 +1,44 @@
 <div class="row">
-    <p>پروفایل</p>
+
+    <div class="col-lg-12">
+        <!-- Horizontal Form -->
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 class="box-title">فرم ویرایش اطلاعات</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form class="form-horizontal">
+                <div class="box-body">
+
+                    <div class="form-group">
+                        <label for="user_full_name" class="col-sm-2 control-label">نام و نام خانوادگی</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="user_full_name" class="form-control" id="user_full_name" placeholder="ایمیل">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email" class="col-sm-2 control-label">ایمیل</label>
+                        <div class="col-sm-10">
+                            <input type="email" name="email" class="form-control" id="email" placeholder="ایمیل">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password" class="col-sm-2 control-label">رمز عبور</label>
+                        <div class="col-sm-10">
+                            <input type="password" name="password" class="form-control" id="password" placeholder="رمز عبور">
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-default">انصراف</button>
+                    <button type="submit" class="btn btn-info pull-right">ورود</button>
+                </div>
+                <!-- /.box-footer -->
+            </form>
+        </div>
 </div>
